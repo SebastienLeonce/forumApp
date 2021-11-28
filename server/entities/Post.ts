279398deda2a952377dea261d0efc52ba2cbf59f
Sébastien LEONCE <sebastienleonce91@gmail.com>
2021-11-28 18:02:53 +0100

    update post model

--- server/entities/Post.ts
+++ server/entities/Post.ts
@@ -3,6 +3,9 @@ export interface IPost {
     title: string;
     content: string;
     author: string;
+    description: string;
+    avatar: string;
+    href: string;
 }
 
 export class Post implements IPost {
@@ -11,23 +14,35 @@ export class Post implements IPost {
     public title: string;
     public content: string;
     public author: string;
+    public description: string;
+    public avatar: string;
+    public href: string;
 
 
     constructor(
         titleOrUser?: string | IPost,
         content?: string,
         author?: string,
+        description?: string,
+        avatar?: string,
+        href?: string,
         id?: number,
     ) {
         if (typeof titleOrUser === 'string' || typeof titleOrUser === 'undefined') {
             this.title = titleOrUser || '';
             this.content = content || '';
             this.author = author || '';
+            this.description = description || '';
+            this.avatar = avatar || '';
+            this.href = href || '';
             this.id = id || -1;
         } else {
             this.title = titleOrUser.title;
             this.content = titleOrUser.content;
             this.author = titleOrUser.author;
+            this.description = titleOrUser.description;
+            this.avatar = titleOrUser.avatar;
+            this.href = titleOrUser.href;
             this.id = titleOrUser.id;
         }
     }
